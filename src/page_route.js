a5a23c48ae1c6c926fb78a15cad98cf484916d7f
Last-SilverLight-End <halfwing1998@gmail.com>
2022-05-11 13:36:15 +0900

    adding page_route

---
+++ src/page_route.js
@@ -0,0 +1,21 @@
+// Import dependencies
+import React, { useRef, useState, useEffect } from "react";
+import {  BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
+
+import Hat_AR from "./Hat_AR";
+import Earring_AR from "./Earring_AR";
+import Necklace_AR from "./Necklace_AR"
+import MainHome from "./MainHome";
+function functions() {
+    return (
+        <Router>
+            <Routes>
+                <Route path = "/" element = {<MainHome/>}/>
+                <Route path = "/Hat_AR" element = {<Hat_AR/>}/>
+                <Route path = "/Necklace_AR" element = {<Necklace_AR/>}/>
+                <Route path = "/Earring_AR" element = {<Earring_AR/>}/>
+
+            </Routes>
+        </Router>
+    )
+}
