3d9a467fc87bff6c950ba6e00af78f04e7cc0c1e
Last-SilverLight-End <halfwing1998@gmail.com>
2022-05-15 16:43:28 +0900

    try adding server and bring images

--- src/MainHome.js
+++ src/MainHome.js
@@ -11,7 +11,7 @@ const Home = () => {
         window.location.href = "/Hat_AR"
     }
     function NecklaceClick(e){
-        window.location.href = "Necklace_AR"
+        window.location.href = "/Necklace_AR"
     }
     
     return (
---
+++ src/file_upload.py
@@ -0,0 +1,75 @@
+import os
+from flask import Flask, render_template, request, session
+from werkzeug.utils import secure_filename
+import logging
+import imghdr
+logger = logging.getLogger('HELLO WORLD')
+
+
+app = Flask(__name__)
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+#여기에서 upload 파일 루트 변경해 줘야 한다.
+UPLOAD_FOLD = './public'
+UPLOAD_FOLDER = os.path.join(APP_ROOT, UPLOAD_FOLD)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# 서버 킬때 기본으로 나오는 부분
+@app.route('/')
+def hello():
+    return 'hello!'
+# test communicate
+@app.route('/api')
+def hellos():
+    return{
+        'hola' : "oh yeah"
+    }
+# change image file how much big or not
+def validate_image(stream):
+    header = stream.read(512)  # 512 bytes should be enough for a header check
+    stream.seek(0)  # reset stream pointer
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
+
+# send upload but must to fix it
+@app.route('/uploader', methods = ['GET','POST'])
+def uploader_file():
+    f = request.files['file']
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+
+    
+    return 'file uploaded successfully'
+
+#bring pulic image by list 
+@app.route('/bring_data', methods = ['GET'])
+def bring_data_image():
+    path = "UPLOAD_FOLD"
+    files = os.listdir(path)
+    newlist = []
+
+    for file in files:
+        if file.endswith(".png"):
+            newlist.append(file)
+        newlist.append(file)
+    
+    print (newlist)
+    return newlist
+    
+
+
+#api testing data
+@app.route('/data')
+def User_data():
+    return {
+        'Name' : "hello",
+        'Age' : "22",
+        "Date" : "x",
+        "Cost" : "1000",
+    }
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
